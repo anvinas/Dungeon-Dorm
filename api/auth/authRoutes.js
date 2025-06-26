3b5476ec611424e5256cd8f68851adaadc35adc4
Adrian Holguin <adrhol12@gmail.com>
2025-06-26 19:28:23 -0400

    fixed filepaths

--- api/auth/authRoutes.js
+++ api/auth/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require ('./authController');
-const verifyToken = require('../../global/verifyToken');
+const verifyToken = require('../global/verifyToken.js');
 
 
 router.post('./register', registerUser);
