7a3d6fefe931433826df5260620eb6a833ae20e8
Adrian Holguin <adrhol12@gmail.com>
2025-06-26 19:19:12 -0400

    Fixed errors with mongoose

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require('express');
 const cors = require('cors');
 const { mongoose } = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("Could not connect to MongoDB", err));
+const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("Could not connect to MongoDB", err));
 
 app.use('/api/auth', require ('./api/auth/authRoutes'));
 
