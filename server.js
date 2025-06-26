d9bc27e388f4e9b3e58ce66e69c745250988a49a
Adrian Holguin <adrhol12@gmail.com>
2025-06-26 19:03:17 -0400

    Bootstrapper API calls

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
+require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-const app = express();
+const { mongoose } = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("Could not connect to MongoDB", err));
+
 app.use(cors());
-app.use(bodyParser.json());
-app.use((req, res, next) =>
-{
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader(
-'Access-Control-Allow-Headers',
-'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-);
-res.setHeader(
-'Access-Control-Allow-Methods',
-'GET, POST, PATCH, DELETE, OPTIONS'
-);
-next();
-});
-app.listen(5000); // start Node + Express server on port 5000
+app.use(express.json());
+
+app.use('/api/auth', require ('./api/auth/authRoutes'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Connected to port ${PORT}`)); 
