351119b7f49a15b6e812d0b8014347b432e4bfc3
Shaquille <shaquillemiller533@gmail.com>
2025-07-02 21:09:59 -0400

    update

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -2,23 +2,31 @@ import styles from "./loginPage.module.css"
 
 function App() {
   return (
-    <div className="relative w-screen h-screen ">
+    <div className="relative w-screen h-screen overflow-hidden">
 
         {/* BG container */}
         <div className="relative w-screen h-screen ">
-            <img src="/img/pixel_bg.png" className={`w-screen max-w-screen h-screen ${styles.bgContainer}`}/>
+            <img src="/img/pixel_bg2.png" className={`w-screen max-w-screen h-screen ${styles.bgContainer}`}/>
 
-            {/* Left Chain */}
-            <img src="/img/pixel_chain.png" className={`h-100 absolute z-2 translate-x-[-50%] translate-y-[-50%] top-[0%] left-[37%] ${styles.chain}`}/>
-            
-            {/* Right Chain */}
-            <img src="/img/pixel_chain.png" className={`h-100 absolute z-2 translate-x-[-50%] translate-y-[-50%] top-[0%] left-[63%] ${styles.chain}`} />
-            
             {/* Banner */}
-            <div className={`absolute h-fit w-full h-fit translate-x-[50%] translate-y-[-50%]  top-[43%] right-[50%] z-3 ${styles.banner}`}>
+            <div className={`absolute h-fit w-full h-fit translate-x-[50%] translate-y-[-50%]  top-[43%] right-[50%] z-3 ${styles.bannerContainer}`}>
                 <div className="relative w-full h-fit flex items-center justify-center">
-                    <img src="/img/wood_texture.png" className="h-120 "/>
-                    <div className={`absolute text-4xl font-bold translate-x-[50%] translate-y-[-50%]  top-[12%] right-[50%] ${styles.headerText}`}>Dungeons and Dorm</div>
+                   {/* Left Chain */}
+                    <img src="/img/chain.png" className={`h-100 absolute z-2  top-[100%] left-[40.5%] ${styles.chain}`}/>
+                    
+                    {/* Right Chain */}
+                    <img src="/img/chain.png" className={`h-100 absolute z-2 top-[0%] left-[59.75%] ${styles.chain}`} />
+                    
+                    {/* Wood container */}
+                    <div className="relative w-fit">
+                      <img src="/img/wood_texture2.png" className={`h-120 z-3 ${styles.banner}`}/>
+
+                       {/* Buttons Container */}
+                        <div className="flex justify-around absolute z-4 translate-x-[-50%] translate-y-[-50%] bottom-[10%] left-[50%] w-full">
+                          <div className="p-3 pr-4 pl-4 rounded-md bg-[#6b8e23] text-[#ffffff] border-1 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:cursor-pointer ">Signup</div>
+                          <div className="p-3 pr-4 pl-4 rounded-md bg-indigo-500 text-[#ffffff] border-black border-1  transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:cursor-pointer hover:bg-indigo-500">Login</div>
+                        </div>
+                    </div>
                 </div>
             </div>
             
