e2a6b12036829d6e6ef17340e702d4dedcb4bc4d
Adrian Holguin <68874345+Fishman1022@users.noreply.github.com>
2025-07-19 03:25:05 -0400

    stats saved

--- api/fight/fightController.js
+++ api/fight/fightController.js
@@ -121,6 +121,25 @@ function applyStatGrowth(className, currentStats, level) //Class name, current s
     return growthFunction(currentStats, level);
 }
 
+async function levelupUser(req, res) {
+    const userId = req.user.userId;
+    const user = await User.findById(userId).populate('Character');
+
+
+    user.xp -= user.toLevelUpXP;
+    user.level += 1;
+
+    const newStats = applyStatGrowth(user.Character.class, user.stats, user.level);
+    user.stats.strength = newStats.strength;
+    user.stats.dexterity = newStats.dexterity;
+    user.stats.intelligence = newStats.intelligence;
+    user.stats.charisma = newStats.charisma;
+    user.maxHP = newStats.maxHP;
+    user.currentHP = newStats.maxHP; // currentHP exists in the database, should be removed but fine for now
+    user.toLevelUpXP = 1000 + (150 * user.level); // Example formula, can be changed
+    await user.save();
+}
+
 exports.levelupUser = async (req, res) => {
     const userId = req.user.userId;
     const user = await UserProfile.findById(userId).populate('Character');
@@ -560,7 +579,7 @@ async function handleEnemyDefeat(userId, enemyId, enemyType) {
     };
     
     // Add XP TO DB
-    if(rewards.readyToLevelUp){
+    if(rewards.readyToLevelUp && user.level < 10){
       user.level+=1;
       user.toLevelUpXP = Math.ceil(user.toLevelUpXP * 1.6);
     }
@@ -598,6 +617,16 @@ async function handleEnemyDefeat(userId, enemyId, enemyType) {
 
     const nextBoss = await setNextBossForUser(user);
     user.currentActiveBoss = nextBoss ? nextBoss._id : null;
+    
+
+    const newStats = applyStatGrowth(user.Character.class, user.stats, user.level);
+    user.stats.strength = newStats.strength;
+    user.stats.dexterity = newStats.dexterity;
+    user.stats.intelligence = newStats.intelligence;
+    user.stats.charisma = newStats.charisma;
+    user.maxHP = newStats.maxHP;
+    user.currentHP = newStats.maxHP; // currentHP exists in the database, should be removed but fine for now
+    
     
     await user.save();
     return rewards;
