28eb542ca697fbd96e07da2e0f1823c09fbb185a
Andrea Vinas <146145204+anvinas@users.noreply.github.com>
2025-07-19 23:55:32 -0400

    make pretty

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const LoginPage(),
+        '/': (context) => CharacterSelectPage(),
         '/select': (context) => CharacterSelectPage(),
         '/play': (context) => const LoginPage(),
         '/bossfight': (context) {
