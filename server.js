000e360a90c66b67b1608037629ce18aab5b9065
Adrian Holguin <adrhol12@gmail.com>
2025-07-24 13:48:00 -0400

    added authentication unit testing

--- server.js
+++ server.js
@@ -42,9 +42,13 @@ app.use('/api/barkeeper', require('./api/barkeeper/barkeeperRoutes'));
 app.use('/api/user', require('./api/user/userRoutes'));
 app.use('/api/fight', require('./api/fight/fightRoutes'));
 
-const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => console.log(`Connected to port ${PORT}`)); 
+if (require.main === module){
+  const PORT = process.env.PORT || 5000;
+  const server = app.listen(PORT, () => console.log(`Connected to port ${PORT}`)); 
 
-server.on('error', (err) => {
-  console.error('Server error:', err);
-});
+  server.on('error', (err) => {
+    console.error('Server error:', err);
+  });
+}
+
+module.exports = app; // Export the app for testing or other purposes
